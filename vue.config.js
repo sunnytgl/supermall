5fad5e3d7a60ca00f16a519ba266651c0c53e4d1
Sunny <654017175@qq.com>
2026-03-30 23:37:33 +0800

    提交代码

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const { resolve } = require("core-js/fn/promise");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      extensions: [],
+      alias: {
+        assets: "@/assets",
+        common: "@/common",
+        components: "@/components",
+        network: "@/network",
+        views: "@/views",
+      },
+    },
+  },
+};
